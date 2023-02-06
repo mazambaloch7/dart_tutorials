fe12b2446c7eda86d7b1585f5049b63a8b534954
AzamKhan <azam.khan@100>
2023-02-07 01:46:16 +0500

    Switch Statement pre and post (Increment & decrements)

---
+++ lib/10_pre_post_increment_decrement.dart
@@ -0,0 +1,21 @@
+void main(){
+
+  int i =5;
+  int j =0;
+
+  //pre increment/decre
+/*
+
+  j = --i;
+  print("Assigning the Value of the i to j: ${j}");
+
+*/
+
+//post incre // decre
+  j = i--;
+  print("Assigning the Value of the i to j: ${j}");
+  print("Assigning the Value of the i : ${i}");
+
+
+
+}
---
+++ lib/9_switch_condition.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  int rollno = 29280;
+
+  switch(rollno){
+    case 123:
+      print("This os wrong number");
+      break;
+
+    case 29280:
+      print("Azam baloch");
+      break;
+
+  default:
+  print("This os default statments");
+
+
+
+
+  }
+
+}
